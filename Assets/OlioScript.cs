3b109918b0ba94bfd28464b17d0b36e789e1a364
tapiotorsti <torstitapi@outlook.comm>
2023-05-15 20:13:13 +0300

    Added character movement to left, right and jumping. Also improvements to physics

--- Assets/OlioScript.cs
+++ Assets/OlioScript.cs
@@ -6,6 +6,13 @@ public class OlioScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float moveSpeed;
+    public float jumpForce;
+    public float jumpDelay;
+
+    bool isGrounded;
+    bool isJumping;
+    float jumpTimer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +23,50 @@ public class OlioScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) == true) {
-            myRigidbody.velocity = moveSpeed * Time.deltaTime * Vector2.left;
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            if (isGrounded)
+            {
+                myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
+            }
+            else if (isJumping)
+            {
+                myRigidbody.AddForce(new Vector2(-moveSpeed, 0f));
+            }
+
             Debug.Log("left");
         }
-        else if (Input.GetKey(KeyCode.UpArrow) == true)
+        else if (Input.GetKey(KeyCode.RightArrow) == true)
         {
-            myRigidbody.velocity = moveSpeed * Time.deltaTime * Vector2.up;
+            if (isGrounded)
+            {
+                myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+            }
+            else if (isJumping)
+            {
+                myRigidbody.AddForce(new Vector2(moveSpeed, 0f));
+            }
+
+            Debug.Log("right");
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) == true && isGrounded && !isJumping && Time.time > jumpTimer)
+        {
+            myRigidbody.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+            isJumping = true;
+            jumpTimer = Time.time + jumpDelay;
             Debug.Log("up");
+        }
+
+        if (isJumping && myRigidbody.velocity.y < 0)
+        {
+            isJumping = false;
         }
     }
 }
